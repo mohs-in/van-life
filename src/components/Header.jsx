96fed6d7937e6de40299246423c69b1c53829648
Mohsin <in.mohsin@outlook.com>
2024-11-19 17:23:38 +0530

    revert header to previous commit to fix responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,27 @@
-import { NavLink, Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 export default function Header() {
-    const activeStyles = {
-        fontWeight: "bold",
-        textDecoration: "underline",
-        color: "#161616"
-    }
-    
     return (
         <header>
-            <Link className="site-logo" to="/">#VanLife</Link>
+            <NavLink className="site-logo" to="/">#VanLife</NavLink>
             <nav>
                 <NavLink 
                     to='/host' 
-                    className={({isActive}) => isActive ? activeStyles : null}
+                    className={({isActive}) => isActive ? 'active-link' : null}
                 >
                     Host
                 </NavLink>
 
                 <NavLink 
                     to="/about"
-                    className={({isActive}) => isActive ? activeStyles : null}
+                    className={({isActive}) => isActive ? 'active-link' : null}
                 >
                     About
                 </NavLink>
                 
                 <NavLink 
                     to="/vans"
-                    className={({isActive}) => isActive ? activeStyles : null}
+                    className={({isActive}) => isActive ? 'active-link' : null}
                 >
                     Vans
                 </NavLink>
